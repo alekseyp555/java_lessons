7547e0fb40fd22bd74fb2e33654895f7bd0dc723
alekseyp555 <park.aleksey@gmail.com>
2018-09-19 17:23:41 +0300

    Создан общий базовый класс для классов-помощников Л.2.8

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,46 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+
+  FirefoxDriver wd;
+
+  private SessionHelper sessionHlelper;
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+
+  public void init() {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    sessionHlelper = new SessionHelper(wd);
+    sessionHlelper.login("admin", "secret");
+  }
+
+  public void stop() {
+    wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
+  }
+
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTests extends TestBase {
+
+  @Test
+  public void testGroupCreation() throws Exception {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("test 1", "test 2", "test 3"));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnToGroupPage();
+   // wd.findElement(By.linkText("Logout")).click();
+   // wd.findElement(By.name("user")).clear();
+   // wd.findElement(By.name("user")).sendKeys("admin");
+  }
+
+}
